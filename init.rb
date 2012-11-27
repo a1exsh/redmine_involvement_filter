b65ca7dd15344827a9dbf6a1d448010aba098e96
Alex Shulgin <ash@commandprompt.com>
2012-11-27 12:00:32 -0800

    Redmine-2.x compatibility.

--- init.rb
+++ init.rb
@@ -1,17 +1,20 @@
 require 'redmine'
-require 'dispatcher'
-
-Dispatcher.to_prepare do
-  require_dependency 'query'
-
-  Query.send(:include, RedmineInvolvementFilter::QueryPatch)
-end
 
 Redmine::Plugin.register :redmine_involvement_filter do
   name 'Redmine Issue Involvement Filter plugin'
   author 'Alex Shulgin <ash@commandprompt.com>'
   description 'A plugin to filter out issues in which the current user is involved.'
-  version '0.0.1'
+  version '0.2.0'
 #  url 'http://example.com/path/to/plugin'
 #  author_url 'http://example.com/about'
+end
+
+prepare_block = Proc.new do
+  Query.send(:include, RedmineInvolvementFilter::QueryPatch)
+end
+
+if Rails.env.development?
+  ActionDispatch::Reloader.to_prepare { prepare_block.call }
+else
+  prepare_block.call
 end
